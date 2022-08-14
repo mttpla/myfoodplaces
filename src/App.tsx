f4894c2298219927ac828364bdc142bc3d8300e4
Matteo Paoli <matteo.paoli@mela.work>
2022-08-14 22:48:15 +0200

    added config secret in .env file

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { GoogleLogin, GoogleLogout } from "react-google-login";
 import { gapi } from "gapi-script";
 import ApiCalendar from "react-google-calendar-api";
 
-
 interface UserInfo {
   imageUrl?: string;
   name?: string;
@@ -12,16 +11,15 @@ interface UserInfo {
 }
 
 const config = {
-    clientId:
-      "949082335257-cnpnbt41a8hh86o9dqpljndm6o0920cg.apps.googleusercontent.com",
-    apiKey: "AIzaSyDQlrJCaFgj8VzQHXA-hHHc1Q_wofPNL0I",
-    scope: "https://www.googleapis.com/auth/calendar",
-    discoveryDocs: [
-      "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-    ],
-  };
+  clientId: process.env.REACT_APP_CLIENTID || "missing key",
+  apiKey: process.env.REACT_APP_APIKEY || "missing key",
+  scope: "https://www.googleapis.com/auth/calendar",
+  discoveryDocs: [
+    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  ],
+};
 
-// const apiCalendar: ApiCalendar = new ApiCalendar(config);
+console.log(process.env.REACT_APP_CLIENTID);
 
 function App() {
   const [userInfo, setUserInfo] = useState<UserInfo | null >(null);
